ec3a1852e2ba96572e082b9e91df558bc02a545e
Mavericks Balitaan <balitaanmavericks@gmail.com>
2023-03-13 18:04:34 +0500

    Fix useEffect with abort controller

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,13 +4,20 @@ function useFetch(url) {
   const [arr, setArr] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
     async function fetchData() {
-      const response = await fetch(url);
+      const response = await fetch(url, { signal });
       const data = await response.json();
       setArr(data);
     }
 
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return arr;
